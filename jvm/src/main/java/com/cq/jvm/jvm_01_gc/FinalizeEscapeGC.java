8892d37ce205970b724e8e82ab38cbb8cc6e8767
metastart <77087020+chenquan-code@users.noreply.github.com>
2022-12-13 17:59:29 +0800

    commit

--- jvm/src/main/java/com/cq/jvm/jvm_01_gc/FinalizeEscapeGC.java
+++ jvm/src/main/java/com/cq/jvm/jvm_01_gc/FinalizeEscapeGC.java
@@ -10,23 +10,26 @@ package com.cq.jvm.jvm_01_gc;
  * 此代码演示了两点：
  * 1.对象可以在被GC时自我拯救。
  * 2.这种自救的机会只有一次，因为一个对象的finalize()方法最多只会被系统自动调用一次
- *
- *
+ * <p>
+ * <p>
  * 代码中有两段完全一样的代码片段，执行结果却是一次逃脱成
  * 功，一次失败了。这是因为任何一个对象的finalize()方法都只会被系统自动调用一次，如果对象面临
  * 下一次回收，它的finalize()方法不会被再次执行，因此第二段代码的自救行动失败了。
  */
 public class FinalizeEscapeGC {
     public static FinalizeEscapeGC SAVE_HOOK = null;
+
     public void isAlive() {
         System.out.println("yes, i am still alive :)");
     }
+
     @Override
     protected void finalize() throws Throwable {
         super.finalize();
         System.out.println("finalize method executed!");
         FinalizeEscapeGC.SAVE_HOOK = this;
     }
+
     public static void main(String[] args) throws Throwable {
         SAVE_HOOK = new FinalizeEscapeGC();
         //对象第一次成功拯救自己
